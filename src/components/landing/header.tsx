bc9aa03d10968e6a58eae77ca5da6a73a16e57e0
Premier Home <contatopremierhome@gmail.com>
2026-01-14 22:35:58 +0000

    retire esse botão (_for element <LinkComponent>_)

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Martini } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
@@ -10,9 +9,6 @@ const Header = () => {
           <Martini className="h-8 w-8 text-primary" />
           <span className="text-xl font-bold">Mestre dos Drinks</span>
         </Link>
-        <Button asChild className="hidden md:flex" size="lg">
-          <Link href="https://www.ggcheckout.com/checkout/v2/VU1EHbdkknKfUXU3JXCx">Obter Receitas</Link>
-        </Button>
       </div>
     </header>
   );
